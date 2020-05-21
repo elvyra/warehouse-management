4dad9a18f4409e1d4e84e1d2ffd715c0fa0ea856
Elvyra Sabaliauskiene <35922895+elvyra@users.noreply.github.com>
2020-05-21 22:14:58 +0300

    Create product functionality (#2)
    * Product interface created
    
    * New product creation added

--- src/components/productCreate/ProductCreate.tsx
+++ src/components/productCreate/ProductCreate.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import ProductForm from "../productForm/ProductForm";
+import { CreateNewProduct } from "../productForm/CreateProductFromFormData";
+import { IProduct } from "../interfaces/interfaces";
+import { GetData, SaveData } from "../localStorage/LocalStorage";
 
 const ProductCreate: React.FC = () => {
-  return <h1>Product Create</h1>;
+  const handleChangeActive = () => {};
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    let list = GetData();
+    let product: IProduct = CreateNewProduct(event);
+    list.push(product);
+    SaveData(list);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <ProductForm handleChangeActive={handleChangeActive} />
+      <input type="submit" value="Submit" />
+    </form>
+  );
 };
 
 export default ProductCreate;
---
+++ src/components/productForm/CreateProductFromFormData.ts
@@ -0,0 +1,17 @@
+import { IProduct } from "../interfaces/interfaces";
+
+export const NewId = () => Math.random().toString(36).substr(2, 9);
+
+export const CreateNewProduct = (event: React.FormEvent<HTMLFormElement>) => {
+  let target = event.currentTarget;
+  let product: IProduct = {
+    id: NewId(),
+    name: ((target.name as unknown) as HTMLInputElement).value,
+    EAN: ((target.ean as unknown) as HTMLInputElement).value,
+    type: Number(((target.type as unknown) as HTMLInputElement).value),
+    weight: Number(((target.weight as unknown) as HTMLInputElement).value),
+    color: Number(((target.color as unknown) as HTMLInputElement).value),
+    active: ((target.active as unknown) as HTMLInputElement).checked,
+  };
+  return product;
+};
