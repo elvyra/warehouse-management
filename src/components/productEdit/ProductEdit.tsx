e9893f6b4a051017c85afcbd01fbc8a733292d5f
Elvyra Sabaliauskiene <35922895+elvyra@users.noreply.github.com>
2020-05-22 14:47:36 +0300

    Edit product functionality (#4)

--- src/components/productEdit/ProductEdit.tsx
+++ src/components/productEdit/ProductEdit.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { IProduct } from "../interfaces/interfaces";
+import { GetData, SaveData } from "../localStorage/LocalStorage";
+import { isNullOrUndefined } from "util";
+import ProductForm from "../productForm/ProductForm";
+import { CreateEditedProduct } from "../productForm/CreateProductFromFormData";
 
-const ProductEdit: React.FC = () => {
-  return <h1>Products Edit</h1>;
+interface PropsType extends RouteComponentProps {
+  id: string;
+}
+
+const ProductEdit: React.FC<PropsType> = (props: PropsType) => {
+  let id: string = (props.match.params as PropsType).id;
+  let items: IProduct[] = GetData();
+  let item: IProduct | undefined = items.find((p) => p.id === id);
+  let index: number = isNullOrUndefined(item) ? -1 : items.indexOf(item);
+
+  const handleChangeActive = () => {};
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    let product: IProduct = CreateEditedProduct(event);
+    let list: IProduct[] = items.slice(0);
+    list.splice(index, 1, product);
+    SaveData(list);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="id" defaultValue={id} hidden />
+      <ProductForm product={item} handleChangeActive={handleChangeActive} />
+      <input type="submit" value="Submit" />
+    </form>
+  );
 };
 
 export default ProductEdit;
