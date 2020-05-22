fda9089cbe01f23eb4e74fb76ee106b94dc4f3c1
Elvyra Sabaliauskiene <elvyra.sabaliauskiene@gmail.com>
2020-05-22 15:03:38 +0300

    Product preview functionality

--- src/components/productPreview/ProductPreview.tsx
+++ src/components/productPreview/ProductPreview.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { NavLink, RouteComponentProps } from "react-router-dom";
+import { IProduct, ProductType, ProductColor } from "../interfaces/interfaces";
+import { GetData } from "../localStorage/LocalStorage";
+import { isNullOrUndefined } from "util";
 
-const ProductsPreview: React.FC = () => {
-  return <h1>Products Preview</h1>;
+interface PropsType extends RouteComponentProps {
+  id: string;
+}
+
+const ProductsPreview: React.FC<PropsType> = (props: PropsType) => {
+  let id: string = (props.match.params as PropsType).id;
+  let items: IProduct[] = GetData();
+  let item: IProduct | undefined = items.find((p) => p.id === id);
+
+  if (isNullOrUndefined(item)) return <p>Product not found</p>;
+  else
+    return (
+      <>
+        <p>
+          <b>Id: </b> {item.id}
+        </p>
+        <p>
+          <b>Name: </b> {item.name}
+        </p>
+        <p>
+          <b>EAN: </b> {item.EAN}
+        </p>
+        <p>
+          <b>Type: </b> {ProductType[item.type]}
+        </p>
+        <p>
+          <b>Weight: </b> {item.weight}
+        </p>
+        <p>
+          <b>Color: </b> {ProductColor[item.color]}
+        </p>
+        <p>
+          <b>Active: </b> {item.active ? "active" : "not active"}
+        </p>
+        <NavLink to="/products">Back to list</NavLink>
+      </>
+    );
 };
 
 export default ProductsPreview;
