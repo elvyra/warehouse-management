f4bfae0f8682f7ca8d57a437dde9e95e64de5870
Elvyra Sabaliauskiene <elvyra.sabaliauskiene@gmail.com>
2020-05-24 09:15:47 +0300

    Product preview updated

--- src/components/productPreview/ProductPreview.tsx
+++ src/components/productPreview/ProductPreview.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, RouteComponentProps } from "react-router-dom";
 import {
   IProduct,
@@ -8,8 +8,10 @@ import {
   IQuantityHistory,
 } from "../interfaces/interfaces";
 import { getItem } from "../localStorage/LocalStorage";
+import { currency, unit, numberOfRecords } from "../interfaces/interfaces";
 import { isNullOrUndefined } from "util";
 import Highchart from "./Highchart";
+import { Tab, Tabs, Card, CardDeck, ListGroup, Badge } from "react-bootstrap";
 
 interface MatchParams {
   id: string;
@@ -17,6 +19,8 @@ interface MatchParams {
 interface PropsType extends RouteComponentProps<MatchParams> {}
 
 const ProductsPreview: React.FC<PropsType> = (props: PropsType) => {
+  const [key, setKey] = useState("details");
+
   let item: IProduct | null | undefined = getItem(props.match.params.id);
 
   return (
@@ -25,63 +29,119 @@ const ProductsPreview: React.FC<PropsType> = (props: PropsType) => {
         <p>Product not found</p>
       ) : (
         <>
-          <p>
-            <b>Id: </b> {item.id}
-          </p>
-          <p>
-            <b>Name: </b> {item.name}
-          </p>
-          <p>
-            <b>EAN: </b> {item.EAN}
-          </p>
-          <p>
-            <b>Type: </b> {ProductType[item.type]}
-          </p>
-          <p>
-            <b>Weight: </b> {item.weight}
-          </p>
-          <p>
-            <b>Color: </b> {ProductColor[item.color]}
-          </p>
-          <p>
-            <b>Active: </b> {item.active ? "active" : "not active"}
-          </p>
-          <Highchart
-            title={"Price history"}
-            subtitle={`Product name: ${item.name} (EAN: ${item.EAN})`}
-            xAxisTitle="Date and time"
-            xAxisCategories={item.priceHistory
-              .map((c) => new Date(c.date).toLocaleString())
-              .reverse()}
-            yAxisTitle="Price"
-            data={item.priceHistory.map((c) => c.price).reverse()}
-          />
-          <p>
-            <b>Price history:</b>
-          </p>
-          {item.priceHistory.map((p: IPriceHistory) => (
-            <p>
-              {p.price} ({new Date(p.date).toLocaleString()})
-            </p>
-          ))}
-          <Highchart
-            title={"Quantity history"}
-            subtitle={`Product name: ${item.name} (EAN: ${item.EAN})`}
-            xAxisTitle="Date and time"
-            xAxisCategories={item.quantityHistory
-              .map((c) => new Date(c.date).toLocaleString())
-              .reverse()}
-            yAxisTitle="Quantity"
-            data={item.quantityHistory.map((c) => c.quantity).reverse()}
-          />
-          <p>
-            <b>Quantity history:</b>
-          </p>
-          {item.quantityHistory.map((p: IQuantityHistory) => (
-            <p>
-              {p.quantity} ({new Date(p.date).toLocaleString()})
-            </p>
-          ))}
+          <Tabs
+            id="controlled-tab-example"
+            activeKey={key}
+            onSelect={(k: string) => setKey(k)}
+          >
+            <Tab eventKey="details" title="Product details">
+              <ListGroup className="mt-4 mb-4">
+                <ListGroup.Item>
+                  <b>Id: </b> {item.id}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <b>Name: </b> {item.name}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <b>EAN: </b> {item.EAN}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <b>Type: </b> {ProductType[item.type]}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <b>Weight: </b> {item.weight}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <b>Color: </b> {ProductColor[item.color]}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <b>Active: </b> {item.active ? "active" : "not active"}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <b>Price: </b> {item.priceHistory[0].price} {currency}
+                  {item.priceHistory[0].price <= 0 ? (
+                    <Badge variant="warning" className="ml-2">
+                      Price inaccuracy
+                    </Badge>
+                  ) : (
+                    <></>
+                  )}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <b>Quantity: </b> {item.quantityHistory[0].quantity} {unit}
+                  {item.quantityHistory[0].quantity === 0 ? (
+                    <Badge variant="warning" className="ml-2">
+                      No in stock
+                    </Badge>
+                  ) : (
+                    <></>
+                  )}
+                </ListGroup.Item>
+              </ListGroup>
+            </Tab>
+            <Tab eventKey="price" title="Price history">
+              <CardDeck className="mt-4 mb-4">
+                <Card style={{ flex: "1 1" }}>
+                  <Card.Body>
+                    <Card.Title>Price history</Card.Title>
+                    <Card.Text>
+                      Last {numberOfRecords} entries are stored in database.
+                    </Card.Text>
+                  </Card.Body>
+                  <ListGroup variant="flush">
+                    {item.priceHistory.map((p: IPriceHistory) => (
+                      <ListGroup.Item>
+                        {p.price} ({new Date(p.date).toLocaleString()})
+                      </ListGroup.Item>
+                    ))}
+                  </ListGroup>
+                </Card>
+                <Card style={{ flex: "2 1" }}>
+                  <Highchart
+                    title={"Price history"}
+                    subtitle={`Product name: ${item.name} (EAN: ${item.EAN})`}
+                    xAxisTitle="Date and time"
+                    xAxisCategories={item.priceHistory
+                      .map((c) => new Date(c.date).toLocaleString())
+                      .reverse()}
+                    yAxisTitle="Price"
+                    data={item.priceHistory.map((c) => c.price).reverse()}
+                  />
+                </Card>
+              </CardDeck>
+            </Tab>
+            <Tab eventKey="quantity" title="Quantity history">
+              <CardDeck className="mt-4 mb-4">
+                <Card style={{ flex: "1 1" }}>
+                  <Card.Body>
+                    <Card.Title>Quantity history</Card.Title>
+                    <Card.Text>
+                      Last {numberOfRecords} entries are stored in database.
+                    </Card.Text>
+                  </Card.Body>
+                  <ListGroup variant="flush">
+                    {item.quantityHistory.map((p: IQuantityHistory) => (
+                      <ListGroup.Item>
+                        {p.quantity} ({new Date(p.date).toLocaleString()})
+                      </ListGroup.Item>
+                    ))}
+                  </ListGroup>
+                </Card>
+                <Card style={{ flex: "2 1" }}>
+                  <Highchart
+                    title={"Quantity history"}
+                    subtitle={`Product name: ${item.name} (EAN: ${item.EAN})`}
+                    xAxisTitle="Date and time"
+                    xAxisCategories={item.quantityHistory
+                      .map((c) => new Date(c.date).toLocaleString())
+                      .reverse()}
+                    yAxisTitle="Quantity"
+                    data={item.quantityHistory.map((c) => c.quantity).reverse()}
+                  />
+                </Card>
+              </CardDeck>
+            </Tab>
+          </Tabs>
           <NavLink to="/products">Back to list</NavLink>
         </>
       )}
