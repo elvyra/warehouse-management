e02233e78ce4a810e1a02aa25f00ba25c1a6611b
Elvyra Sabaliauskiene <elvyra.sabaliauskiene@gmail.com>
2020-05-25 11:56:48 +0300

    cleanup

--- src/components/toasts/ToastCustom.tsx
+++ src/components/toasts/ToastCustom.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Toast } from "react-bootstrap";
-import { delayToast } from "../interfaces/interfaces";
-import { IToast, ToastType } from "../interfaces/interfaces";
+import { IToast, delayToast, ToastType } from "../interfaces/interfaces";
 
 type PropsType = {
   toast: IToast;
@@ -11,20 +10,18 @@ const ToastCustom: React.FC<PropsType> = ({ toast }: PropsType) => {
   const [show, setShow] = useState(true);
 
   return (
-    <>
-      <Toast
-        show={show}
-        onClose={() => setShow(false)}
-        delay={delayToast}
-        autohide
-        className={`toast-${ToastType[toast.type]}`}
-      >
-        <Toast.Header>
-          <strong className="mr-auto">{toast.title}</strong>
-        </Toast.Header>
-        <Toast.Body>{toast.text}</Toast.Body>
-      </Toast>
-    </>
+    <Toast
+      show={show}
+      onClose={() => setShow(false)}
+      delay={delayToast}
+      autohide
+      className={`toast-${ToastType[toast.type]}`}
+    >
+      <Toast.Header>
+        <strong className="mr-auto">{toast.title}</strong>
+      </Toast.Header>
+      <Toast.Body>{toast.text}</Toast.Body>
+    </Toast>
   );
 };
 
