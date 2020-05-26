106d31358b29bdd02aca8e39959c1b5f1bfb89a3
Elvyra Sabaliauskiene <elvyra.sabaliauskiene@gmail.com>
2020-05-26 16:22:18 +0300

    with navbar layout added

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -8,24 +8,51 @@ import ProductEdit from "../productEdit/ProductEdit";
 import { Container } from "react-bootstrap";
 import ToastsProvider from "../../context/ToastsProvider";
 import Toasts from "../toasts/Toasts";
-import Navbar from "../navbar/Navbar";
+import NavBar from "../navbar/Navbar";
+
+const WithNavbarLayout = (props: any) => {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <Container>{props.children}</Container>
+      </main>
+    </>
+  );
+};
+
+const WithNavBar = ({
+  component: Component,
+  ...rest
+}: {
+  exact: true;
+  path: string;
+  component: any;
+}) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        <WithNavbarLayout>
+          <Component {...props} />
+        </WithNavbarLayout>
+      )}
+    />
+  );
+};
 
 const App: React.FC = () => {
   return (
     <Router>
       <ToastsProvider>
         <Toasts />
-        <Navbar />
-        <Container>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-
-            <Route exact path="/products" component={ProductTable} />
-            <Route exact path="/products/create" component={ProducCreate} />
-            <Route path="/products/:id/edit" component={ProductEdit} />
-            <Route path="/products/:id" component={ProductPreview} />
-          </Switch>
-        </Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <WithNavBar exact path="/products" component={ProductTable} />
+          <Route exact path="/products/create" component={ProducCreate} />
+          <Route path="/products/:id/edit" component={ProductEdit} />
+          <Route path="/products/:id" component={ProductPreview} />
+        </Switch>
       </ToastsProvider>
     </Router>
   );
